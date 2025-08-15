4ee2b48e6fb647359b4ab2cd03cdcef84383caa1
Timothy-src <aeroltimothy@gmail.com>
2025-08-15 14:51:28 +0800

    added placeholders or initial codes for the container of recruitment page

--- lib/presentations/landing/tabs/recruits_screen.dart
+++ lib/presentations/landing/tabs/recruits_screen.dart
@@ -13,8 +13,88 @@ class _RecruitScreenState extends State<RecruitScreen> {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 231, 240, 251),
       body: SafeArea(
-        child: SingleChildScrollView(padding: EdgeInsets.all(24.0)),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              _buildHeader(),
+              const SizedBox(height: 22),
+              _buildContainer(),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Recruitment",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 33,
+            color: Color(0xFF1E2D59),
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Manage you team growth and track recruitment progress",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 13,
+            color: Colors.grey[700],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: EdgeInsets.all(16.0),
+          height: 160,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            color: Color(0xFF1E2D59),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 4),
+                spreadRadius: 0,
+                blurRadius: 10,
+                color: Colors.black12,
+              ),
+            ],
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Recruits",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(width: 18),
+                      Text("This month", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
